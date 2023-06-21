6b2ab097a2fdf7503804ab663c578de2d9e616e6
Rahmenzs <reyjet01@gmail.com>
2023-06-21 16:41:16 +0800

    addad RPS

--- resources/views/pia/recommended_privacy_solutions.blade.php
+++ resources/views/pia/recommended_privacy_solutions.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.general_layout')
 
-@section('title', 'Recommended Privacy Solutions')
+@section('title', 'VII. Recommended Privacy Solutions')
 
 @section('content')
 
@@ -8,6 +8,52 @@
     <p>Welcome to the Recommended Privacy Solutions</p>
 
     <p>!! Under Construction !!</p>
+    <p>From the risks stated in the previous section, identify the recommended solution or mitigation 
+        measures. You can cite your existing controls to treat the risks in the same column. </p>
+    
+        <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover text-nowrap">
+                  <thead>
+                    <tr>
+                     <th>Recommended Solutions (Please provide justification)</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    </tr>
+                    <tr>
+                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    </tr>
+                    <tr>
+                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    </tr>
+                    <tr>
+                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    </tr>
+                    <tr>
+                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    </tr>
+                    <tr>
+                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    </tr>
+                    <tr>
+                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    </tr>
+                    
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          
 
     <form action="proceed_to_end" method="post">
         @csrf
